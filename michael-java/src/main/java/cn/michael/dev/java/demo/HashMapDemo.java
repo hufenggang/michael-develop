ae9d0ad5728974b8d6e9b5c7104d4d754214586a
MikealHu <13035144322@163.com>
2021-06-11 00:15:02 +0800

    feat: 新增demo

---
+++ michael-java/src/main/java/cn/michael/dev/java/demo/HashMapDemo.java
@@ -0,0 +1,14 @@
+package cn.michael.dev.java.demo;
+
+import java.util.HashMap;
+
+/**
+ * Created by hufenggang on 2021/5/31.
+ */
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+
+    }
+}
---
+++ michael-java/src/main/java/cn/michael/dev/java/demo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by hufenggang on 2021/5/31.
+ */
+package cn.michael.dev.java.demo;
